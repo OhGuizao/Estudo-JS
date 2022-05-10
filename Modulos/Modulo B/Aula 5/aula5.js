83276881bf0f5486c16e62bd3afbcbdfb1ff470b
guilherme vieira <guizinho.vfs@gmail.com>
2022-05-09 22:08:09 -0300

    Variáveis e Tipos de dados

---
+++ Modulos/Modulo B/Aula 5/aula5.js
@@ -0,0 +1,9 @@
+var n1 = 5
+var n2 = 8.5
+var n3 = 15
+var s1 = "JavaScript" // Atribuição com aspas duplas
+var s2 = 'Curso em Video' // Atribuição com aspas simples 
+var s3 = `Guanabara` //Atribuição com crase
+
+
+typeof s3 // Pesquisa do tipo da variável
